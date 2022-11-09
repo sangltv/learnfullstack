9f018fb8998094792f91c1b43690f8a61438f3ca
sangltv <sangltv@proton.me>
2022-11-09 11:44:01 +0000

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import {Div, Button, H4} from "@thaysang/mui"
+import {Div, Button, H4, Img, P} from "@thaysang/mui"
 import useSWR from 'swr'
-
 const fetcher = (url) => fetch(url).then(res => res.json())
 
 const App = () => {
   const {data,error} = useSWR("https://reqres.in/api/users?page=1", fetcher)
   if (error) return(<H4>error</H4>)
   if (!data) return (<H4>Loading...</H4>)
+  console.log(data)
   return(
     <Div>
-      <H4>Title</H4>
-      {data.data.map}
+      {data.data.map((item,index) => <Div key={index} size={120} display="flex">
+        <Img src={item.avatar} size={100}/>
+        <P>{item.first_name}  {item.last_name}</P>
+      </Div>)}
       <Button variant="contained">TEST</Button>
     </Div>
   )
