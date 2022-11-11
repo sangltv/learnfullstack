2e8516b72c95fed927b53a5e604a7041215e2bde
sangltv <sangltv@proton.me>
2022-11-11 11:53:17 +0000

    login

--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -1,2 +1,25 @@
-const Login = () => <p>LOGIN</p>
+import {Div, Button} from "@thaysang/mui"
+import {FormContainer, TextFieldElement} from 'react-hook-form-mui'
+import axios from "axios"
+const Login = () => {
+    return (
+        <FormContainer
+            defaultValues={{name: ''}}
+            onSuccess={data => {
+                axios.post("https://nodejs-fake-api.herokuapp.com/login",data).then(
+                    r => {
+                        console.log(r)
+                    }
+                )
+            }}
+        >
+            <Div display="flex" flexDirection="column">
+                <TextFieldElement name="username" label="username" required/>
+                <TextFieldElement type="password" name="password" label="password" required/>
+                <Button type="submit">Login</Button>
+            </Div>
+        </FormContainer>
+    )
+}
+
 export default Login
